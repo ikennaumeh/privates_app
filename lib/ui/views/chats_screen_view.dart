54af0a7dd3bfebd66eb208673b2b2f14af89df35
Ikenna Umeh <kennaumeh@gmail.com>
2022-08-04 18:24:04 +0100

    Fixed chat

--- lib/ui/views/chats_screen_view.dart
+++ lib/ui/views/chats_screen_view.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:privates_app/core/decorations/color_palette.dart';
@@ -16,9 +19,31 @@ class ChatScreenView extends StatefulWidget {
 }
 
 class _ChatScreenViewState extends State<ChatScreenView> {
+
+  late TextEditingController _message;
+
+  @override
+  void initState() {
+    super.initState();
+    _message = TextEditingController();
+  }
+
+  Future<void> _send(String text) async {
+    if (kDebugMode) {
+      final user = FirebaseAuth.instance.currentUser;
+      if(user != null){
+        Message message = Message(author: user.displayName ?? "Anonymous", authorId: user.uid,
+            photoUrl: user.photoURL ?? "https://placehold.it/100x100", timestamp: Timestamp.now().millisecondsSinceEpoch, value: text);
+        await FirebaseFirestore.instance.collection("chat_messages").add(message.toJson());
+      }
+      text = '';
+      _message.clear();
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Palette.grey,
       appBar: AppBar(
         leading: const CustomBackButton(),
         title: Column(
@@ -68,42 +93,65 @@ class _ChatScreenViewState extends State<ChatScreenView> {
       ),
       body: Column(
         children: [
-          Expanded(child: GestureDetector(
-              onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-              child: Container(color: Colors.red,))),
-          Container(
-            color: Colors.blue,
-            padding: const EdgeInsets.symmetric(vertical: 5),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5,),
-                    child: TextField(
-                      minLines: 1,
-                      maxLines: 3,
-                      textInputAction: TextInputAction.send,
-                      style: const TextStyle(
-                        height: 1,
-                      ),
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Palette.textFieldFillColor,
-                        hintText: "Message",
-                        contentPadding: const EdgeInsets.only(left: 20, right: 20, top: 10,bottom: 10),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(20),
-                        )
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection("chat_messages").orderBy("timestamp").snapshots(),
+              builder: (context, snapshot){
+                if(snapshot.hasData){
+                  if(snapshot.data!.docs.isEmpty){
+                    return const Center(child: Text("No messages"),);
+                  }
+                  return MessageWall(messages: snapshot.data!.docs);
+                }
+                return const Center(child: CircularProgressIndicator(),);
+              },
+            ),
+          ),
+          ColoredBox(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5,),
+                      child: TextField(
+                        controller: _message,
+                        minLines: 1,
+                        maxLines: 10,
+                        textInputAction: TextInputAction.send,
+                        onSubmitted: (value){
+                          if(value.isNotEmpty){
+                            _send(value);
+                          }
+                        },
+                        style: const TextStyle(
+                          height: 1,
+                        ),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Palette.textFieldFillColor,
+                          hintText: "Type a message",
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 5),
-                  child: SvgPicture.asset("assets/svg/send-media.svg", width: 35,),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(right: 5),
+                    child: SvgPicture.asset(
+                      "assets/svg/send-media.svg",
+                      width: 35,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
 
@@ -111,4 +159,138 @@ class _ChatScreenViewState extends State<ChatScreenView> {
       ),
     );
   }
+}
+
+class MessageWall extends StatelessWidget {
+  final List<QueryDocumentSnapshot> messages;
+
+  const MessageWall({Key? key,required this.messages}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      shrinkWrap: true,
+      itemCount: messages.length,
+      padding: const EdgeInsets.only(top: 5),
+      itemBuilder: (c,i){
+        Message message = Message.fromJson(messages.elementAt(i).data() as Map<String, dynamic>);
+        final user = FirebaseAuth.instance.currentUser;
+
+        if(user != null && user.uid == message.authorId){
+          return Dismissible(
+              onDismissed: (_){
+                _onDelete(messages.elementAt(i).id);
+              },
+              key: ValueKey(message.timestamp),
+              child: ChatMessage(index: i, data: message));
+        }
+        return ChatMessageOther(index: i, data: message);
+      },
+      separatorBuilder: (c,i){
+        return SizedBox(height: DeviceScaler().scale(5),);
+      },
+    );
+  }
+
+  Future<void> _onDelete(String docId) async {
+    await FirebaseFirestore.instance.collection("chat_messages").doc(docId).delete();
+  }
+}
+
+class ChatMessageOther extends StatelessWidget {
+  final int index;
+  final Message data;
+  final bool hasPadding;
+  const ChatMessageOther({Key? key,required this.index,required this.data, this.hasPadding = false}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 5,bottom: 5, left: 10, right: 10),
+      child: Row(
+
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            constraints: const BoxConstraints(
+              maxWidth: 200,
+            ),
+            margin: const EdgeInsets.only(left: 10,),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+            decoration: BoxDecoration(
+              color: Palette.white,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Text("${data.value}"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  final int index;
+  final Message data;
+  const ChatMessage({Key? key,required this.index,required this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 5,bottom: 5, left: 10, right: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            constraints: const BoxConstraints(
+              maxWidth: 200,
+            ),
+            margin: const EdgeInsets.only(right: 10,),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+            decoration: BoxDecoration(
+              color: Palette.primary.withOpacity(.5),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Text("${data.value}"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+class Message{
+  final String? author;
+  final String? authorId;
+  final String? photoUrl;
+  final int? timestamp;
+  final String? value;
+
+  Message({
+   this.author,
+   this.authorId,
+   this.photoUrl,
+   this.timestamp,
+   this.value,
+ });
+
+  static Message fromJson(Map<String, dynamic> json) => Message(
+    author: json['author'],
+    authorId: json['author_id'],
+    photoUrl: json['photo_url'],
+    timestamp: json['timestamp'],
+    value: json['value']
+  );
+
+  Map<String, dynamic> toJson ()=> {
+    "author": author,
+    "author_id": authorId,
+    "photo_url": photoUrl,
+    "timestamp": timestamp,
+    "value": value,
+  };
+
 }
